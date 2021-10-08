9c91b10e777179a80e60894fdfd06024cbbc0e7f
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-10-08 17:43:25 +0530

    Changed from insta to youtube

--- youtube-sub-counter/src/youtube.py
+++ youtube-sub-counter/src/youtube.py
@@ -5,5 +5,8 @@ from boltiot import Bolt
 
 bolt_api_key = "0f05f3f5-7704-43fa-ab27-4efd68da9639"  # Replace the string with the API key 
 device_id = "BOLT8024024"    # Replace the string with your Bolt ID
+youtube_api = "AIzaSyDyNfLg1j0wRmoHuRD_WmJSp9i5WgcMpQ8"
+channel_id = "UClqdtHozFJQsFI46mcsrchw"
 
 mybolt = Bolt(bolt_api_key, device_id)      # Connecting to the cloud using the API key and Bolt ID
+
