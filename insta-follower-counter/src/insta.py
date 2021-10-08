71046be25a0730c7237cf9b0b3ccfdfdcef98579
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-10-08 17:22:25 +0530

    Removed insta plan

---
+++ insta-follower-counter/src/insta.py
@@ -0,0 +1,9 @@
+import requests
+import json
+import time
+from boltiot import Bolt
+
+bolt_api_key = "0f05f3f5-7704-43fa-ab27-4efd68da9639"  # Replace the string with the API key 
+device_id = "BOLT8024024"    # Replace the string with your Bolt ID
+
+mybolt = Bolt(bolt_api_key, device_id)      # Connecting to the cloud using the API key and Bolt ID
