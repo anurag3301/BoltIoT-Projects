af12205a910942b4078dc3483e887cf94e91ed8c
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-10-08 18:23:52 +0530

    Tested the LCD screen

--- youtube-sub-counter/src/main.cpp
+++ youtube-sub-counter/src/main.cpp
@@ -1,9 +1,22 @@
 #include<Arduino.h>
 
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
+
 void setup(){
-  Serial.begin(9600);
+    lcd.init();
+    lcd.backlight();
+    lcd.print("");
+    Serial.begin(9600);
+    Serial.setTimeout(50);
 }
 
 void loop(){
-
+      lcd.setCursor(0, 0);
+      lcd.print("Subs: ");
+      lcd.setCursor(0, 1);
+      lcd.print("Viewes: ");
 }
+
