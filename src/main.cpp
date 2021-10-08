c25f6143ebf2b06facd53fb83ab9444c2ef2ce6b
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-10-08 16:14:22 +0530

    Turn on and off the computer

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,26 @@
 void setup() {
   Serial.begin(9600);
   pinMode(11, INPUT);
+  pinMode(12, OUTPUT);
+  
+
 }
 
 void loop() {
-  int s ;
+
+  digitalWrite(12, HIGH);
+  delay(500);
+  digitalWrite(12, LOW);
+  delay(500);
+  digitalWrite(12, HIGH);
+  delay(5000);
   
-  while(Serial.available()==0){
-  }
-  s = Serial.read();
-  int x = digitalRead(11);
-  Serial.flush();
-  Serial.print(x);
+  
+  // while(Serial.available()==0){
+  // }
+  // s = Serial.read();
+  // int x = digitalRead(11);
+  // Serial.flush();
+  // Serial.print(x);
   
 }
