6c4f37d351d55a51acfcc7bd8fb0682bb74f1c6e
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-10-13 19:32:45 +0530

    Removed API key from Youtube-sub-counter

--- youtube-sub-counter/src/youtube.py
+++ youtube-sub-counter/src/youtube.py
@@ -2,13 +2,9 @@ import requests
 import json
 import time
 from boltiot import Bolt
+import config
 
-bolt_api_key = "0f05f3f5-7704-43fa-ab27-4efd68da9639"  # Replace the string with the API key 
-device_id = "BOLT8024024"    # Replace the string with your Bolt ID
-youtube_api = "AIzaSyDyNfLg1j0wRmoHuRD_WmJSp9i5WgcMpQ8"
-channel_id = "UCDzhFuVYou1D8w1ABZo3b9A"
-
-mybolt = Bolt(bolt_api_key, device_id)      # Connecting to the cloud using the API key and Bolt ID
+mybolt = Bolt(config.bolt_api_key, config.device_id)      # Connecting to the cloud using the API key and Bolt ID
 
 def get_youtube_data(key, id):
     url  = "https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + id\
@@ -23,7 +19,7 @@ def get_youtube_data(key, id):
 
 
 while True:
-    data = get_youtube_data(youtube_api, channel_id)
+    data = get_youtube_data(config.youtube_api, config.channel_id)
     if data:
         subs = str(data["subs"])
         views = str(data["views"])
