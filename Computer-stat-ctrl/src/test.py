afa8205f77303057f254a69d802c6ca9007633d5
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-10-23 09:29:50 +0530

    Added full integration in discord

--- Computer-stat-ctrl/src/test.py
+++ Computer-stat-ctrl/src/test.py
@@ -9,5 +9,15 @@ mybolt = Bolt(os.environ["API_KEY"], os.environ["DEVICE_ID"]) #Create bolt objec
 
 response = json.loads(mybolt.serialWrite(1))
 print(response)
-webhook = DiscordWebhook(url=os.environ["DISCORD_URL"], content=str(response))
-webhook.execute()
+
+response = json.loads(mybolt.serialRead("1"))
+print(response)
+
+if(response["value"] == "0"):
+    webhook = DiscordWebhook(url=os.environ["DISCORD_URL"], content="The computer is offf...")
+    webhook.execute()
+
+if(response["value"] == "1"):
+    webhook = DiscordWebhook(url=os.environ["DISCORD_URL"], content="The computer is working")
+    webhook.execute()
+
