546ffad2ef19ec996bae00b146c1aac908e59509
Anurag Kumar Singh <anuragkumar07102003@gmail.com>
2021-10-23 09:24:20 +0530

    Using Discord instead of telegram

--- Computer-stat-ctrl/src/test.py
+++ Computer-stat-ctrl/src/test.py
@@ -3,10 +3,11 @@ import time
 import requests
 import json
 import os
-
+from discord_webhook import DiscordWebhook
 
 mybolt = Bolt(os.environ["API_KEY"], os.environ["DEVICE_ID"]) #Create bolt object
 
 response = json.loads(mybolt.serialWrite(1))
 print(response)
-
+webhook = DiscordWebhook(url=os.environ["DISCORD_URL"], content=str(response))
+webhook.execute()
